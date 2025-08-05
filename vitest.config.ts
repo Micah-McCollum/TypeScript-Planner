ab26b2012cf90cf3628cd1292a9ed508c3046984
duckJaguar <duckJaguar@duck.com>
2025-08-04 23:10:43 -0400

    tests/Test-Suite-Setup-Smoketest

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vitest/config";
+import  path from "path";
+
+export default defineConfig({
+    resolve: {
+    alias: {
+      "@assets":     path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@contexts":   path.resolve(__dirname, "src/contexts"),
+      "@hooks":      path.resolve(__dirname, "src/hooks"),
+      "@pages":      path.resolve(__dirname, "src/pages"),
+      "@services":   path.resolve(__dirname, "src/services"),
+      "@styles":     path.resolve(__dirname, "src/styles"),
+      "@utils":      path.resolve(__dirname, "src/utils"),
+      "@shared":     path.resolve(__dirname, "src/shared"),
+      "@tests":      path.resolve(__dirname, "src/__tests__"),
+    },
+  },
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./setupTests.ts",
+    },
+});
